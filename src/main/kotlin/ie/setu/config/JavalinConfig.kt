9a5c2347e10adc0d778f1090c037a0a4bbe609ce
bijo-at-688165709689 <bijo@cordovacloud.com>
2024-10-17 23:35:00 +0100

    Listening on remote port

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -4,16 +4,31 @@ import io.javalin.Javalin
 import ie.setu.controllers.HealthTrackerController
 
 class JavalinConfig{
-
+    private fun getRemoteAssignedPort(): Int {
+        val remotePort = System.getenv("PORT")
+        return if (remotePort != null) {
+            Integer.parseInt(remotePort)
+        } else 7001
+    }
     fun startJavalinService():Javalin {
 
-        val app = Javalin.create().apply{exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+        val app = Javalin.create().apply {
+            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
             error(404) { ctx -> ctx.json("404 - Not Found") }
-        }.start(7001)
+        }.start(getRemoteAssignedPort())
 
-registerRoutes(app)
+        registerRoutes(app)
         return app
     }
+
+
+//        val app = Javalin.create().apply{exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+//            error(404) { ctx -> ctx.json("404 - Not Found") }
+//        }.start(7001)
+//
+//registerRoutes(app)
+//        return app
+//    }
 
     private fun registerRoutes(app: Javalin) {
         app.get("/api/users", HealthTrackerController::getAllUsers)
