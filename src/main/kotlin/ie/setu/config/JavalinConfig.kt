9ec1d5df6f4537d6fcbfd6938a3ca2054d779050
bijo-at-688165709689 <bijo@cordovacloud.com>
2024-11-14 12:18:20 +0000

    daily goals feature api s and unit tests

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -1,7 +1,10 @@
 package ie.setu.config
 
+import ie.setu.controllers.DailyGoalsController
 import io.javalin.Javalin
 import ie.setu.controllers.UserController
+import ie.setu.utils.jsonObjectMapper
+import io.javalin.json.JavalinJackson
 
 class JavalinConfig{
     private fun getRemoteAssignedPort(): Int {
@@ -12,10 +15,24 @@ class JavalinConfig{
     }
     fun startJavalinService():Javalin {
 
-        val app = Javalin.create().apply {
-            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+
+        val app = Javalin.create {
+//            this line is taken from chatgpt to globally set the jsonObjectMapper for serialization and deserialization
+            config ->
+            config.jsonMapper(JavalinJackson(jsonObjectMapper()))
+        }.apply {
+            exception(Exception::class.java) { e, ctx ->
+                e.printStackTrace()
+                ctx.status(500).json("Internal Server Error")
+            }
             error(404) { ctx -> ctx.json("404 - Not Found") }
         }.start(getRemoteAssignedPort())
+
+//        val app = Javalin.create().apply {
+//
+//            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+//            error(404) { ctx -> ctx.json("404 - Not Found") }
+//        }.start(getRemoteAssignedPort())
 
         registerRoutes(app)
         return app
@@ -31,11 +48,21 @@ class JavalinConfig{
 //    }
 
     private fun registerRoutes(app: Javalin) {
-        app.post("/api/users/user-login",UserController::loginUser)
+
+//        user-table-apis-for-login-register-etc
+
+        app.post("/api/users/login",UserController::loginUser)
         app.get("/api/users", UserController::getAllUsers)
         app.get("/api/users/{user-id}", UserController::getUserById)
-        app.post("/api/users/register-new-user", UserController::registerNewUser)
-        app.delete("/api/users/delete-user/{user-id}", UserController::deleteUser)
-        app.put("/api/users/update/{user-id}",UserController::updateUser)
+        app.post("/api/users/signup", UserController::registerNewUser)
+        app.delete("/api/users/{user-id}", UserController::deleteUser)
+        app.put("/api/users/{user-id}",UserController::updateUser)
+
+        //        daily-goals-apis-for-creating-daily-goals
+
+        app.post("/api/users/{user-id}/daily-goals", DailyGoalsController::addDailyGoalsToUser)
+        app.get("/api/users/{user-id}/daily-goals",DailyGoalsController:: getAllDailyGoalsByUserId)
+        app.delete("/api/users/{user-id}/daily-goals/{goal-id}", DailyGoalsController::deleteDailyGoalsFromUser)
+        app.put("/api/users/{user-id}/daily-goals/{goal-id}",DailyGoalsController::updateDailyGoalsForUser)
     }
 }
--- src/main/kotlin/ie/setu/domain/User.kt
+++ src/main/kotlin/ie/setu/domain/User.kt
@@ -2,7 +2,6 @@ package ie.setu.domain
 
 data class User(val user_id:Int,val name:String, val email:String, val password:String)
 
-//data class UserTest(val user_id:Int, val name:String, val email:String, val password:String)
 
 data class PayloadLogin(
     val email: String,
